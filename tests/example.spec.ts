3276e893a56ad09de4e40b4642ca7f76aa69e938
IlarionHalushka <hilarion.galushka@gmail.com>
2023-11-12 16:50:01 +0200

    refactor: remove wait 5s

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -5,8 +5,6 @@ test("has title", async ({ page }) => {
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
-  // timeout 5 secs
-  await page.waitForTimeout(5000);
 });
 
 test("get started link", async ({ page }) => {
